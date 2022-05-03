2d8d160d9d8d1ab0ce712f440a267f2716ae0df4
unknown <samueloctavianus97@gmail.com>
2022-05-03 14:49:49 +0700

    update

--- simon-game.js
+++ simon-game.js
@@ -1,10 +1,45 @@
 buttonColors = ["green", "red","yellow", "blue"];
+userClickedPattern = [];
 gamePattern = [];
+let activeAudio = "";
+let buttons = document.querySelectorAll(".btn");
 
 function nextSequence(){
     var randomNum = Math.floor(Math.random()*4)
     randomChosenColor = buttonColors[randomNum];
     gamePattern.push(randomChosenColor)
+    playSound(randomChosenColor);
     console.log(randomChosenColor);
     console.log(gamePattern);
+}
+
+//Button Clicks
+for(i = 0; i < buttons.length; i++){
+    let userChosenColor = buttons[i].id;
+    userChosenColorArr = buttons[i];
+
+    buttons[i].addEventListener("click", function(){
+        userClickedPattern.push(userChosenColor);
+
+        //play audio if button clicked
+        playSound(userChosenColor);
+        animatePress(userChosenColorArr);
+
+        console.log(this);
+        console.log(userChosenColor);
+        console.log(userChosenColorArr);
+        })
+}
+
+//ButtonPress
+function animatePress(currentColor){
+    currentColor.classList.add("pressed");
+    console.log(currentColor);
+}
+
+
+//Play Sound function
+function playSound(name){
+    var name = new Audio(`./sounds/${name}.mp3`);
+        name.play();
 }
